cf5c865e0423c000d87126cae9f3260fc08aa947
Hans Engelbrecht <hengelbrecht@godaddy.com>
2020-04-25 00:17:03 -0700

    completed required portion of assignment

--- working_query.sql
+++ working_query.sql
@@ -12,3 +12,70 @@ SELECT * FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
 ORDER BY hire_date;
 
+-- 3. List the manager of each department with the following
+--    information. 
+--   1. department number
+--   2. department name
+--   3. employee number
+--   4. last name
+--   5. first name 
+--   6. start date
+--   7. end date 
+
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, 
+        e.first_name, dm.from_date, dm.to_date
+FROM department AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no=dm.dept_no
+INNER JOIN employees AS e 
+ON e.emp_no=dm.emp_no
+
+-- 4. List the department of each employee with the following 
+--    information: 
+--    employee number, last name, first name, and department name.
+
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e 
+INNER JOIN dept_emp AS de 
+ON e.emp_no=de.emp_no
+INNER JOIN department AS d 
+ON d.dept_no=de.dept_no;
+
+-- 5. List all employees whose first name is "Hercules" and last
+--    names begin with "B".
+
+SELECT emp_no, first_name, last_name, birth_date, gender, hire_date
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- 6. List all employees in the Sales department, including their 
+--    employee number, last name, first name, and department number
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name, de.dept_no
+FROM employees AS e
+INNER JOIN dept_emp AS de 
+ON e.emp_no=de.emp_no
+INNER JOIN department AS d
+ON d.dept_no=de.dept_no
+WHERE d.dept_no = 'd007';
+
+-- 7. List all employees in the Sales and Development departments, 
+--    including their employee number, last name, first name, and 
+--    department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de 
+ON e.emp_no=de.emp_no
+INNER JOIN department AS d
+ON d.dept_no=de.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+-- 8. In descending order, list the frequency count of employee last names, 
+--    i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name)
+FROM employees 
+GROUP BY last_name
+ORDER BY count desc;
+
--- working_schema.sql
+++ working_schema.sql
@@ -4,27 +4,22 @@ CREATE TABLE department(
 	dept_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE dept_emp(
-	team_change SERIAL PRIMARY KEY, 
+CREATE TABLE dept_emp( 
 	emp_no INTEGER NOT NULL,
 	dept_no VARCHAR(30) NOT NULL,
 	from_date DATE,
-	to_date DATE
+	to_date DATE,
+	id SERIAL PRIMARY KEY,
+	FOREIGN KEY (dept_no) REFERENCES department(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
-CREATE TABLE dept_manager(
-	boss_change SERIAL PRIMARY KEY,
-	dept_no VARCHAR(30) NOT NULL,
-	emp_no INTEGER NOT NULL,
-	from_date DATE,
-	to_date DATE
-);
 
 CREATE TABLE employees(
 	emp_no INTEGER PRIMARY KEY NOT NULL,
+	birth_date VARCHAR(30),
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
-	birth_date VARCHAR(30),
 	gender VARCHAR(10),
 	hire_date DATE
 ); 
@@ -33,15 +28,55 @@ CREATE TABLE salaries(
 	emp_no INTEGER NOT NULL,
 	salary INTEGER,
 	from_date DATE,
-	to_date DATE
+	to_date DATE,
+	id SERIAL PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
-CREATE TABLE titles(
-	emp_no INTEGER PRIMARY KEY NOT NULL,
-	title VARCHAR(30),
-	from_date DATE,
-	to_date DATE
+CREATE TABLE title_list(
+	title_id INTEGER PRIMARY KEY,
+	title_name VARCHAR(30) NOT NULL 
 );
+
+
+CREATE TABLE titles(
+	emp_no INTEGER NOT NULL,
+	from_date DATE,
+	to_date DATE,
+	title_id INTEGER NOT NULL,
+	id SERIAL PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (title_id) REFERENCES title_list(title_id)	
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(30) NOT NULL,
+	emp_no INTEGER NOT NULL,
+	from_date DATE,
+	to_date DATE,
+	id SERIAL PRIMARY KEY
+);
+
+-- Adding, then expirementing with ALTER TABLE to add foreign keys to dept_manager table. 
+ALTER TABLE dept_manager
+ADD FOREIGN KEY (dept_no) REFERENCES department(dept_no);
+
+SELECT 
+  TABLE_NAME,
+  COLUMN_NAME,
+  CONSTRAINT_NAME
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE TABLE_NAME = 'salaries';
+
+ALTER TABLE dept_manager
+ADD FOREIGN KEY (dept_no) REFERENCES department(dept_no);
+
+ALTER TABLE dept_manager
+ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
+
+ALTER TABLE dept_manager
+DROP CONSTRAINT dept_manager_dept_no_fkey1;
+
 
 DROP TABLE department
 DROP TABLE dept_emp
